e1ee8f9465b2f67d9093f35ff1c0e92d582998ea
kumargaurav007 <kumargaurav1073@gmail.com>
2026-01-21 22:56:04 +0530

    User Routes, Stats Routes & Controllers created

--- backend/src/routes/stats.route.js
+++ backend/src/routes/stats.route.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
+import { getStats } from "../controller/stats.controller.js";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Stats route With GET method");
-});
+router.get("/", protectRoute, requireAdmin, getStats);
 
 export default router;
